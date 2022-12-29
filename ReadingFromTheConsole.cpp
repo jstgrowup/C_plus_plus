7bcea662a1efc426ce4c8d865c43a990baa834f7
jstgrowup <deysubham999@gmail.com>
2022-12-29 19:08:13 +0530

    Switched the video

--- ReadingFromTheConsole.cpp
+++ ReadingFromTheConsole.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main()
 {
+    cout << "enter a value";
+    int value = 0;
+    cin >> value;
+    // >> this is a stream extraction operator
+    cout << value;
     return 0;
 }
---
+++ p1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world" << endl;
+    cout << "hello i am subham" << endl;
+    int a, b, c;
+
+    return 0;
+}
